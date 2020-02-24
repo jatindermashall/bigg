15dd4030d7f8c082289b421d39bad5bf9044423d
jatinder kumar <jatindermashall@gmail.com>
2020-02-24 22:33:40 +0530

    Change home page

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Routes from './components/routing/Routes';
-
+import Profiles from './components/profiles/Profiles';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
